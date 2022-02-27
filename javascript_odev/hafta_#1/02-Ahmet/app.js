d42005ed3da698c8d627fb727d0da0fc845d92d1
Ahmet DOLU <ahmetdolu@gmail.com>
2022-02-27 17:00:52 +0100

    01-Ahmet-JS-1-Hafta-Odevi-1

---
+++ javascript_odev/hafta_#1/02-Ahmet/app.js
@@ -0,0 +1,19 @@
+
+console.log("30 dan 0 ra kadar 2şer azalan sayılar");
+console.log("-------------------------------------");
+
+for (i=30; i>=0; i=i-2){
+    console.log("Azalan Sayı :" + i);
+}
+console.log("-------------------------------------");
+
+
+
+console.log("0dan 50ye kadar tek sayılar ");
+console.log("-------------------------------------");
+
+for(i=1;i<=50; i=i+2){
+    console.log("Artan Tek Sayılar :" + i);
+}
+console.log("-------------------------------------");
+

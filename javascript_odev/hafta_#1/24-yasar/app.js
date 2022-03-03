8d593d76c278470ef0bb89cb7cce84056fbc80ea
ysrkznc <ysrkznc@gmail.com>
2022-03-03 21:46:27 +0100

    ödev düzenleme

--- javascript_odev/hafta_#1/24-yasar/app.js
+++ javascript_odev/hafta_#1/24-yasar/app.js
@@ -3,6 +3,8 @@ for (var i = 30; i > -1; i = i-2) {
 }
 
 
-for (var i = 1; i <= 49; i = i+2) {
-    console.log("i sayısı :" + i)
+for (var i = 0; i <= 50; i++) {
+    if(i % 2 == 1) {
+        console.log("tek i sayısı :" + i)
+    }
 }

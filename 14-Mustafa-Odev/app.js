d515e14b2f8d5e0f2c1322f84fba26f44b16c674
Mustafa DURHAN <mustafa.durhan1980@gmail.com>
2022-03-03 22:27:10 +0100

    14-MustafaJavaOdev

---
+++ 14-Mustafa-Odev/app.js
@@ -0,0 +1,14 @@
+console.log('Mustafa Ödev');
+
+console.log("-------------")
+
+for (var i = 30; i >= 0; i= i-2) {
+        console.log("İkişer Azalan Sayı : " + i)
+}
+
+console.log("-------------")
+
+for (var i = 0; i <= 50; i++) {
+    if(i % 2)
+    console.log("Tekli Sayılar : " + i)
+}

706e54f8167b121d4727bae3ee1f320c99b5eed9
B-Mahmut <98771239+B-Mahmut@users.noreply.github.com>
2022-03-03 13:55:34 +0100

    -03-Mahmut

---
+++ javascript_odev/hafta_#1/03-Mahmut/app.js
@@ -0,0 +1,8 @@
+for (var sayi = 30; sayi >= 0; sayi = sayi - 2) {
+  console.log("Azalan Sayi :" + sayi);
+}
+
+for (var i = 1; i <= 50; i++)
+  if (i % 2 == 1) {
+    console.log("Tek Sayi :" + i);
+  }

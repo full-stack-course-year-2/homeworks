96ab6a109489ed593733b200b924644933b6150a
nevbahar-20 <yer4534@gmail.com>
2022-03-09 21:00:10 +0100

    20-senol

---
+++ javascript_odev/hafta_#1/20-senol/app.js
@@ -0,0 +1,17 @@
+for (var i=30;  i>=0;  i=i-2) {
+    console.log("i sayisi:" + i);
+}
+
+/* birinci yöntem */
+
+for (var i=1; i<=50;  i=i+2) {
+    console.log("i sayisi:" +i);
+}
+
+/* ikinci yöntem*/
+
+for (var i=0 ;i<=50; i++){
+    if(i%2==1){
+        console.log("i sayisi :" + i)
+    }
+}

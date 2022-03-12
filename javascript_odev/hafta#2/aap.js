d72aeaf1eeb12e9323c86d756d31c6d5bf1f46b3
B-Mahmut <98771239+B-Mahmut@users.noreply.github.com>
2022-03-13 00:19:16 +0100

    Update aap.js

--- javascript_odev/hafta#2/aap.js
+++ javascript_odev/hafta#2/aap.js
@@ -1,3 +1,8 @@
+var sonuc = topla(3, 5);
+var sonuc1 = cikar(69, 35);
+var sonuc2 = bolme(0, 55);
+var sonuc3 = carpma(0, 37);
+
 function topla(sayi1, sayi2) {
   var result = sayi1 + sayi2;
   return result;
@@ -9,23 +14,32 @@ function cikar(sayi1, sayi2) {
 
 function bolme(sayi1, sayi2) {
   var result = sayi1 / sayi2;
+  if (sayi1 <= "0") {
+    result = "sonuc hatali:  " + " ilk sayi sifir olamaz!!!";
+  } else {
+    result = "sonuc dogru:   " + result;
+  }
+
   return result;
 }
 
 function carpma(sayi1, sayi2) {
   var result = sayi1 * sayi2;
+  if (sayi1 * sayi2 == 0) {
+    result =
+      "sonuc sifir:    " + " Birsayinin sifirla carpimi her zaman sifirdir";
+  } else {
+    result = "sonuc dogru:" + result;
+  }
   return result;
 }
 
-var sonuc = topla(3, 5);
-var sonuc1 = cikar(69, 35);
-var sonuc2 = bolme(90, 15);
-var sonuc3 = carpma(25, 12);
-
-console.log(sonuc);
-console.log(sonuc1);
+console.log(9 * 81);
 console.log(sonuc2);
+console.log(900 / 6);
+console.log(25 + 99);
 console.log(sonuc3);
+console.log(101 * 202);
 
 function araba(tercih) {
   var result;

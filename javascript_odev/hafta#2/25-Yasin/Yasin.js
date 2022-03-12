bb159a8f586bdd7d4fbb7748da59fb091ae83137
Harriery <yasin.utuk.nl@gmail.com>
2022-03-12 22:27:09 +0100

    degisiklikler yapildi

--- javascript_odev/hafta#2/25-Yasin/Yasin.js
+++ javascript_odev/hafta#2/25-Yasin/Yasin.js
@@ -1,5 +1,6 @@
 var islemSonucu = {
-    hata: '0 a bolunmez'
+    hata_1 : '0 a bolunmez',
+    hata_2 : 'deger giriniz',
 }
 
 
@@ -8,12 +9,12 @@ function topla(sayi_1, sayi_2){
     var result =sayi_1 + sayi_2;
     
 
-    if (sayi_1 >= 0 && sayi_2>= 0 && sayi_1 != undefined && sayi_2 != undefined){
+    if (sayi_1 >= 0 || sayi_1<=0 && sayi_2>= 0 || sayi_2<=0 && sayi_1 != null && sayi_2 != null ){
 
         var toplamaSonuc = topla( );
 
     }else{
-        result = false
+        result =islemSonucu.hata_2
     }
 
     return result;
@@ -23,11 +24,11 @@ function topla(sayi_1, sayi_2){
 function cikar(sayi_1, sayi_2){
     var result =sayi_1 - sayi_2;
 
-    if (sayi_1 >= sayi_2 && sayi_1 != undefined && sayi_2 != undefined){
+    if (sayi_1 >= 0 || sayi_1<=0 && sayi_2>= 0 || sayi_2<=0 && sayi_1 != null && sayi_2 != null){
         var cikarmaSonuc = cikar( );
 
     }else{
-        result = false
+        result = islemSonucu.hata_2
     }
 
     return result;
@@ -37,11 +38,11 @@ function cikar(sayi_1, sayi_2){
 function carp(sayi_1, sayi_2){
     var result =sayi_1 * sayi_2;
 
-    if (sayi_1 >= 0 && sayi_2>= 0 && sayi_1 != undefined && sayi_2 != undefined){
+    if (sayi_1 >= 0 || sayi_1<=0 && sayi_2>= 0 || sayi_2<=0 && sayi_1 != null && sayi_2 != null){
         var carpmaSonuc = carp();
 
     }else{
-        result = false
+        result = islemSonucu.hata_2
     }
 
     return result;
@@ -50,28 +51,34 @@ function carp(sayi_1, sayi_2){
 function bol(sayi_1, sayi_2){
     var result =sayi_1 / sayi_2;
 
-    if (sayi_1 >= 0 && sayi_2 > 0 && sayi_1 != undefined && sayi_2 != undefined){
+    if (sayi_2 != 0 && sayi_1 >= 0 || sayi_1<=0 && sayi_2 > 0 || sayi_2 < 0 ){
         var bolmeSonuc = bol();
 
     }else{
-        result = islemSonucu.hata
+        result = islemSonucu.hata_1 
     }
 
+    if (sayi_1 == null && sayi_2 == null){
+      
+        result = islemSonucu.hata_2
+    }else {
+        bolmeSonuc = bol();
+    }
     return result;
 }
 
 
 
-toplamaSonuc = topla(2, 2);
+toplamaSonuc = topla(0, 2);
 console.log(toplamaSonuc);
 
-cikarmaSonuc = cikar(2 , 1);
+cikarmaSonuc = cikar(-3 , 1);
 console.log(cikarmaSonuc);
 
-carpmaSonuc = carp(2 , 5);
+carpmaSonuc = carp(2 , -4);
 console.log(carpmaSonuc);
 
-bolmeSonuc = bol(2, 0);
+bolmeSonuc = bol(2 ,0);
 console.log(bolmeSonuc);
 
 

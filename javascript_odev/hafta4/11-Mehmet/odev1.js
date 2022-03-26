81caf2f73d8d362df2e1157f1d01ba5881a09651
mehmeteminisvec <freebutterfly3506@gmail.com>
2022-03-26 15:27:05 +0100

    4.haftaodevı

---
+++ javascript_odev/hafta4/11-Mehmet/odev1.js
@@ -0,0 +1,154 @@
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//***MAP***
+//1. Tüm isimlerin dizisini al
+sonuc=characters.map(item=>{
+    return item.name
+})
+console.log(sonuc);
+
+//2. Tüm yüksekliklerin dizisini al
+sonuc=characters.map(item=>{
+    return item.height
+})
+console.log(sonuc);
+
+//3. Yalnızca ad ve yükseklik özelliklerine sahip nesne dizisini alın
+sonuc=characters.map(item=>{
+    return{
+        adı:item.name,
+        yukseklık:item.height,
+    }
+})
+console.log(sonuc);
+//4. Tüm ilk isimlerin dizisini al
+sonuc = characters.map(item => item.name.split(" ") [0]);
+console.log(sonuc);
+
+
+
+//***REDUCE***
+//1. Tüm karakterlerin toplam kütlesini alın
+sonuc=characters.reduce((acc,ıtem)=>{
+    acc=acc+ıtem.mass;
+    return acc;
+   },0);
+   console.log('toplam Kutle= ' +sonuc);
+//2. Tüm karakterlerin toplam yüksekliğini alın
+sonuc=characters.reduce((acc,ıtem)=>{
+    acc=acc+ıtem.height;
+    return acc;
+   },0);
+   console.log('toplam Yukseklık= ' +sonuc);
+//3. Göz rengine göre toplam karakter sayısını alın
+sonuc=characters.reduce((acc,value)=>{
+    if(acc[value.eye_color]){
+        acc[value.eye_color]=acc[value.eye_color]+1;
+    }else{
+        acc[value.eye_color]=1;
+    }
+    return acc;
+},{})
+console.log(sonuc);
+//4. Tüm karakter adlarındaki toplam karakter sayısını alın
+sonuc = characters.reduce((acc, item) => acc + item.name.length, 0);
+console.log(sonuc);
+
+
+//***FILTER***
+//1. 100'den büyük kütleye sahip karakterler alın
+sonuc=characters.filter(item=>item.mass>100)
+console.log(sonuc);
+//2. 200'den az yüksekliğe sahip karakterler alın
+sonuc=characters.filter(item=>item.height<200)
+console.log(sonuc);
+//3. Tüm erkek karakterleri al
+sonuc=characters.filter(item=>item.gender=='male')
+console.log(sonuc);
+//4. Tüm kadın karakterleri al
+sonuc=characters.filter(item=>item.gender=='female')
+console.log(sonuc);
+
+
+
+//***SORT***
+// //1. Kütleye göre sırala
+result=characters.sort((a,b)=>a.mass-b.mass);
+console.log(result);
+// //2. Yüksekliğe göre sırala
+sonuc=characters.sort((emın,mehmet)=>emın.height-mehmet.height);
+console.log(sonuc);
+//3. İsme göre sırala
+characters.sort((ısım,soyısım)=>{
+    if(ısım.name<soyısım.name) {
+     return -1}else{
+     return 1;}
+})
+console.log(characters)
+//4. Cinsiyete göre sırala
+sonuc=characters.sort((a,b)=>{
+    if(a.gender=='female'){
+        return -1;
+    }else{
+        return 1;
+    }
+})
+console.log(sonuc)
+
+//***EVERY***
+//1. Her karakterin mavi gözleri var mı?
+sonuc=characters.every(item=>item.eye_color == 'blue')
+console.log(sonuc);
+//2. Her karakterin kütlesi 40'tan fazla mı?
+sonuc=characters.every(item=>item.mass > 40);
+console.log(sonuc);
+//3. Her karakter 200'den kısa mı?
+sonuc=characters.every(item=>item.height <200);
+console.log(sonuc);
+//4. Her karakter erkek mi?
+sonuc=characters.every(item=>item.gender == 'male');
+console.log(sonuc);
+
+
+//***SOME***
+//1. En az bir erkek karakter var mı?
+sonuc=characters.some(ıtem=>ıtem.gender=='male')
+console.log(sonuc);
+//2. Mavi gözlü en az bir karakter var mı?
+sonuc=characters.some(ıtem=>ıtem.eye_color =='blue')
+console.log(sonuc);
+//3. 210'dan uzun en az bir karakter var mı?
+sonuc=characters.some(item=>item.height > 210)
+console.log(sonuc);
+//4. Kütlesi 50'den az olan en az bir karakter var mı?
+sonuc=characters.some(item=>item.mass<50);
+console.log(sonuc);
---
+++ javascript_odev/hafta4/11-Mehmet/odev3.js
@@ -0,0 +1,50 @@
+const orders = [
+    { orderId: '123', customerId: '123', deliveryDate: '01-01-2020', delivered: true, items: [
+        { productId: '123', price: 55 },
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '234', customerId: '234', deliveryDate: '01-02-2020', delivered: false, items: [
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '345', customerId: '234', deliveryDate: '05-01-2020', delivered: true, items: [
+        { productId: '567', price: 30 },
+        { productId: '678', price: 80 },
+    ]},
+    { orderId: '456', customerId: '345', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '789', price: 12 },
+        { productId: '890', price: 90 },
+    ]},
+        { orderId: '578', customerId: '456', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '901', price: 43 },
+        { productId: '123', price: 55 },
+    ]},
+];
+
+// 1) Teslim edilmeyen 234 ID'li müşteri için siparişlerin bir listesini alın.
+sonuc = orders.filter(yenı => (yenı.customerId == '234' && yenı.delivered == false ));
+console.log(sonuc[0].items);
+
+// 2) Sipariş edilen ürünlerin toplam fiyatı ile her siparişte yeni bir özellik oluşturun.
+const toplamfıyat = orders.map(item => {
+    return {
+            ...item,
+            toplamfıyat: item.items.reduce((acc, item) => {
+            acc =acc+ item.price;
+            return acc;
+            } , 0)
+    }
+});
+console.log(toplamfıyat);
+
+
+// 3) Tüm siparişler teslim edildi mı?
+result = orders.every(css=>css.delivered == true);
+console.log(result);
+
+
+// 4) '123' kimlikli müşteri sipariş verdi mi?
+sonuc = orders.some ( item=>item.orderId=='123' )
+console.log(sonuc);
+// 5) 123 kimlikli ürün satıldı mı?
+sonuc = orders.some(sıparıs => sıparıs.items.some(item => item.productId == "123"));
+console.log(sonuc)

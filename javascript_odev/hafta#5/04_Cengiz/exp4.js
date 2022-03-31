4d9761735990208cbd18864e3ebba7e75f077c78
murat <merdo.net@gmail.com>
2022-03-31 22:07:28 +0200

    4.soru cozumu

--- javascript_odev/hafta#5/04_Cengiz/exp4.js
+++ javascript_odev/hafta#5/04_Cengiz/exp4.js
@@ -1,21 +1,31 @@
 // 4) Yorumlar dizisindeki her yoruma kullanıcının adını ve soyadını ekleyin
 
-const { comments, users } = require('./data');
+const { comments, users } = require("./data");
 
-let newList = [];
+// let newList = [];
 
-const comList = comments.forEach(function (params, i) {
-    if (params.userId == users[i].id) {
-        newList.push(
-            {
-                userId: params.userId,
-                text: params.text,
-                firstname: users[i].firstName,
-                lastname: users[i].lastName
+// const comList = comments.forEach(function (params, i) {
+//     if (params.userId == users[i].id) {
+//         newList.push(
+//             {
+//                 userId: params.userId,
+//                 text: params.text,
+//                 firstname: users[i].firstName,
+//                 lastname: users[i].lastName
 
-            })
-    }
+//             })
+//     }
 
-})
+// })
 
-console.log(newList)
+// console.log(newList)
+
+const newComments = comments.map((item) => {
+  const user = users.find((user) => user.id === item.userId);
+  return {
+    ...item,
+    name: `${user.firstName} ${user.lastName}`,
+  };
+});
+
+console.log(newComments);

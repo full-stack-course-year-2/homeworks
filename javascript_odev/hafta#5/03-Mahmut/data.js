81f90cd7c220098c5e955e8295e92601bc6561e4
B-Mahmut <98771239+B-Mahmut@users.noreply.github.com>
2022-04-02 13:16:46 +0200

    03// 5. Hafta odev

--- javascript_odev/hafta#5/03-Mahmut/data.js
+++ javascript_odev/hafta#5/03-Mahmut/data.js
@@ -70,7 +70,20 @@ const comments = [
   { userId: "6ee904be-e3b0-41c9-b7a2-5a0233c38e4c", text: "Great stuff!" },
 ];
 
-module.exports = {
-  users,
-  comments,
-};
+module.exports = { users, comments };
+
+// Odev.js deki users ve commentsler export edilecek,
+
+// 5 soru da ayrı ayrı .js dosyalarında olsun (exp1.js , exp2.js ... vb),
+
+// açılan her dosyaya da export edilenler require edilecek
+
+// 1) Madison Marshall'ın kullanıcı kimliği nedir?
+
+// 2) İlk yorumu kim yazdı (ilk yorumun yorumlar dizisinin 0 konumunda olduğu varsayılarak)
+
+// 3) Hangi kullanıcı 'Tamam harika teşekkürler' yorumunu yaptı?
+
+// 4) Yorumlar dizisindeki her yoruma kullanıcının adını ve soyadını ekleyin
+
+// 5) Yorum yapmayan kullanıcıların listesini alın

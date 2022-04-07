96a1c763bf08478f9c80fad0f449256b7b5994e2
Reşat ARIKAN <60408130+resatarikan@users.noreply.github.com>
2022-04-07 18:25:31 +0100

    Jest Test uygulamasi tamamlandi

---
+++ javascript_odev/hafta#6/18-Ra/exp3.test.js
@@ -0,0 +1,7 @@
+
+const result = require('./exp3');
+
+test("User who wrote 'Tamam harika teşekkürler' is 'Jar Burke'", () => {
+    expect(result.userWhoWroteTamam.firstName).toBe('Jar');
+    expect(result.userWhoWroteTamam.lastName).toBe('Burke');
+});

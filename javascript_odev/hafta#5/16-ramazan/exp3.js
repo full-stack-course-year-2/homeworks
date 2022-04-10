837ac28142b917befc5f983be7f7edbdfb6ebd53
RamazanKacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2022-04-10 11:25:28 +0200

    Revert "ramo16"
    This reverts commit 94e81f1647e4485bb6a2725499ff1355b71da26e.

--- javascript_odev/hafta#5/16-ramazan/exp3.js
+++
@@ -1,9 +0,0 @@
- // 3) Hangi kullanıcı 'Tamam harika teşekkürler' yorumunu yaptı?
-
- const { users, comments } = require('./data.js');
-
- const userId = comments.filter(mess => mess.text === "OK great thanks")[0].userId;
- 
- const whichUser = users.filter(user => user.id === userId)[0];
-
- console.log(whichUser.firstName + " " + whichUser.lastName);
--- javascript_odev/hafta#5/16-ramazan/exp5.js
+++
@@ -1,9 +0,0 @@
- // 5) Yorum yapmayan kullanıcıların listesini alın
- const { users, comments } = require('./data.js');
-
-const notCommented = users.filter(user => comments.filter(comment => comment.userId === user.id).length === 0);
-
-
-notCommented.forEach((user, index) => {
-    console.log((index + 1).toString() + "- " + user.firstName + " " + user.lastName);
-});

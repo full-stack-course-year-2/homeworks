fc2fcb994df0c6e6673a9925cc7ac4b0218a9703
nevbahar-20 <yer4534@gmail.com>
2022-04-10 12:18:47 +0200

    son
    aaaa

---
+++ javascript_odev/hafta#5/20-senol/exp5.js
@@ -0,0 +1,25 @@
+// 5) Yorum yapmayan kullanıcıların listesini alın
+const { users, comments } = require('./data.js');
+
+
+    for( let i = 0; i < users.length; i++)
+    {
+        let varmi = false;
+        for ( let x = 0; x < comments.length; x++)
+        { 
+
+            if (comments[x].userId.includes(users[i].id))
+            {
+                varmi = true;
+                break;
+            }
+        }
+
+        if (varmi == false)
+        {
+            console.log(users[i].firstName);
+        }
+        
+    }
+
+    

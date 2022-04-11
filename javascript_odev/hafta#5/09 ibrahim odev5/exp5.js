22d031b7c060c6a4a30aa625aa7b658bc32cc872
09ibrahimcavusoglu <hhicoglu@gmail.com>
2022-04-11 15:53:49 +0200

    09 ibrahim

--- javascript_odev/hafta#5/09 ibrahim odev5/exp5.js
+++ javascript_odev/hafta#5/09 ibrahim odev5/exp5.js
@@ -1,6 +1,14 @@
 // 5) Yorum yapmayan kullanıcıların listesini alın
-const {users,comments} = require('./data.js');
 
-sonuc = comments.some((item => item = users.id == comments.userId)? -1 : 1);
+const {users, comments} = require('./data.js');
+
+let result = [];
+const commentsId = comments.map(comment => comment.userId)
+users.forEach(user => {
+   if (commentsId.includes(user.id) == false) result.push(user);  
+}) 
+
+const sonuc = users.filter(user => commentsId.includes(user.id) == false);
 
 console.log(sonuc);
+

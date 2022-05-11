17a68497cee33b00c4305216dd497d8a8aefa9b9
RamazanKacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2022-05-12 01:21:09 +0200

    ramazan

---
+++ javascript_odev/hafta#10/16-ramazan/app1.js
@@ -0,0 +1,9 @@
+let baslik = document.querySelector("#satir1");
+
+baslik.innerHTML = "Bos Beles Dunya";
+baslik.style.color = "red";
+
+let baslik1  = document.getElementById("satir2");
+
+baslik1.innerHTML = "Yalan Dunya Inanma";
+baslik1.style.color = "blue";
